89b5ad74fa256738505f8d6707faf2338236808b
Tema1313 <tolkouchebaivse@mail.ru>
2026-09-15 11:11:37 +0700

    fix: object-contain

--- src/pages/cats/ui/cat-personal-info/CatPhotos.tsx
+++ src/pages/cats/ui/cat-personal-info/CatPhotos.tsx
@@ -34,7 +34,7 @@ export const CatPhotos: FC<ICatPhotosProps> = (props) => {
 												onError={() => setLoaded(true)}
 												decoding="async"
 												draggable={false}
-												className={`w-full h-full object-cover transition-opacity duration-200 ${
+												className={`w-full h-full object-contain transition-opacity duration-200 ${
 													loaded ? "opacity-100" : "opacity-0"
 												}`}
 											/>
